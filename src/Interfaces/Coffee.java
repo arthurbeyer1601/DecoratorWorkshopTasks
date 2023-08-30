63870d7c73239d36d8a1b9f8ffa7ee38b057db9e
ARBEYER <arthur.beyer@otto.de>
2023-08-30 12:26:18 +0200

    Initial commit

---
+++ src/Interfaces/Coffee.java
@@ -0,0 +1,6 @@
+package Interfaces;
+
+public interface Coffee {
+    double getCost();
+    String getIngredients();
+}
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import Classes.Espresso;
+import Classes.Latte;
+import Decorator.MilkDecorator;
+import Decorator.SugarDecorator;
+import Interfaces.Coffee;
+
+public class Main {
+    public static void main(String[] args) {
+//        Coffee espresso = new Espresso();
+//        System.out.println("Espresso: " + espresso.getIngredients() + ", Cost: $" + espresso.getCost());
+//
+//        Coffee latte = new Latte();
+//        System.out.println("Latte: " + latte.getIngredients() + ", Cost: $" + latte.getCost());
+//
+//        Coffee latteWithMilkAndSugar = new SugarDecorator(new MilkDecorator(new Latte()));
+//        System.out.println("Latte with Milk and Sugar: " + latteWithMilkAndSugar.getIngredients() + ", Cost: $" + latteWithMilkAndSugar.getCost());
+    }
+}
